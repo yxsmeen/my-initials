5add704f1ffbf2786edb70a92da53f1dfb7c739a
yasmeen 3 <89415904+yxsmeen@users.noreply.github.com>
2021-09-08 19:34:37 -0400

    Add files via upload

---
+++ MyInitials1.py
@@ -0,0 +1,31 @@
+#***********************************************************
+#	Purpose:	Getting started with Python. Creating and running 
+#			Python.
+#			
+#	Input:		None
+#	Output:		Displaying a short paragraph about yourself and 
+#			your initials in large block letters. 
+#	Author:		<Yasmeen Hart>
+#	Date:		<Aug 20, 2020>
+#	Class:		<CS 1010>
+#	Program:	#1 (MyInitials1.py)
+#***********************************************************
+def myParagraph():
+        print("Hi! I'm Yasmeen, a computer science major here at VSU.")
+        print("I enjoy adventuring, finding good food, and painting.")
+        print("I chose VSU because it was close to home and has a beautiful campus.")
+        print("I've been interested in computer science since I was young so this major means a lot to me.")
+def initials():
+        print("YYY      YYY  EEEEEEEEEE   HHH       HHH")
+        print("  YYY  YYY    EEE          HHH       HHH")
+        print("    YY YY     EEE          HHH       HHH")
+        print("     YYY      EEE          HHH       HHH")
+        print("     YYY      EEEEEEE      HHHHHHHHHHHHH")
+        print("     YYY      EEE          HHH       HHH")
+        print("     YYY      EEE          HHH       HHH")
+        print("     YYY      EEE          HHH       HHH")
+        print("     YYY      EEEEEEEEEE   HHH       HHH")
+
+myParagraph()
+initials()
+	
